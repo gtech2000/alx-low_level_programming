942ccba0ac1b8b6e312f7747ed56af24de9b9271
“gtech2000” <wiggga111@gmail.com>
2023-03-19 11:36:41 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that print a line with put function 
- * Return: 0  (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-Puts("\"Programming is like building a multilingual puzzle");
+puts(""Programming is like building a multilingual puzzle");
 return (0);
 }
