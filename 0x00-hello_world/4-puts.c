6f32eb4e103a0280bf22205f80b28a5c2a123069
“gtech2000” <wiggga111@gmail.com>
2023-03-20 11:36:50 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
  * main - Entry point
+ *
  * Return: Always 0 (Success)
  */
-
+int main(void)
 {
-	Puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
