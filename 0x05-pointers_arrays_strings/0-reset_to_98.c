92bad1027d4d7c9f7ad66419c5cf43bcc76d9b56
“gtech2000” <wiggga111@gmail.com>
2023-03-28 00:48:38 -0700

    0-reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointer to an int and updates
+ * the vaule it point to to 98
+ *
+ * @n:- the vaule to be updated to 98
+ * Return: Always 0
+ */
+void reset_to_98(int *n)
+{
+
+
+*n = 98;
+}
