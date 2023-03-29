ee123e651804d1c533b6801bde4a2cce1b12f642
“gtech2000” <wiggga111@gmail.com>
2023-03-29 06:10:34 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * add - Adds two vaules
+ * @n1: The first number to be computed
+ * @n2: The second number to be computed
+ * Return: Integer vaule
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
