e9fd72b324ad137edbea096c0f4d94333e1d1d18
“gtech2000” <wiggga111@gmail.com>
2023-04-03 11:03:32 -0700

    g8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_arrey - prints n elements of an array of integers
+ * print_array - prints n elements of an array of integers
  * followed by a new line
  * @a: array to be printed
  * @n: number of elements to print
