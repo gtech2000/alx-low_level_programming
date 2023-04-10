213a1cf1111ba4eff00df19d1668a9949ff517b6
“gtech2000” <wiggga111@gmail.com>
2023-04-10 08:46:04 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - that return the length of a string
+ * @s: string
+ *
+ * Return: success.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
