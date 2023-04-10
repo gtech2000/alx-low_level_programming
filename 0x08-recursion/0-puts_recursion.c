9416345acc9535f53a057f1fbb3459e2a5fd38d6
“gtech2000” <wiggga111@gmail.com>
2023-04-10 07:37:26 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,9 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s = 0, s <= 1)
+	if (*s == 0)
 	{
-		_putchar('\n');
-		return;
+		_puts_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
 }
