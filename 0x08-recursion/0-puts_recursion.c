b4720dc39af475bc97e315842303c5b1a4788df5
“gtech2000” <wiggga111@gmail.com>
2023-04-13 17:38:57 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,7 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
-		_puts_recursion(s + 1);
-		_putchar(*s);
+		_puchar('\n');
+		return;
 	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - return square root of a number
+ * @n: integer
+ *
+ * Return: -1.
+ */
+	
