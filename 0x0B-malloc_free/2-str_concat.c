0ac7ea10859dc75c258e9bce84b2874b308cb171
“gtech2000” <wiggga111@gmail.com>
2023-04-14 07:39:39 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -4,7 +4,7 @@
  * str_concat - get ends of input and add together for size
  * @s1: input one to concat
  * @s2: input two to concat
- * Return: concat of s1 and s2
+ * Return: success
  */
 char *str_concat(char *s1, char *s2)
 {
@@ -14,15 +14,9 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 
 		s1 = "";
-	if (s2 != NULL)
-	{
-		while (s2[] != '\0')
-			len2++;
-	}
-	else
-	{
+	if (s2 == NULL)
 		s2 = "";
-	}
+		i = ci = 0;
 	while (s1[i] != '\0')
 		i++;
 	while (s2[ci] != '\0')
