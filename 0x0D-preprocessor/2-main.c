892a9ac1d2c9a00243780add3ac9c807c8bc457c
“gtech2000” <wiggga111@gmail.com>
2023-04-17 17:42:41 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the name of the program
+ * that it was complied from and a new line.
+ * Return: 0;
+ */
+int  main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
