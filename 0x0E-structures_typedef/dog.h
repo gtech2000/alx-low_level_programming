fb5e752a49f738b0087a24ba1b2926f34b902784
“gtech2000” <wiggga111@gmail.com>
2023-04-19 15:52:11 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - define a new type to dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
